0772ddbb91d82c24ef1f38da3515adf159544bfd
kiven <kiven986@gmail.com>
2023-03-04 20:19:57 +0900

    no message

---
+++ TestIfElse.java
@@ -0,0 +1,16 @@
+public class TestIfElse {
+	public static void main(String[] args) {
+		int x = 35;
+	   if(x == 10) {
+		System.out.println("x的值是10");
+	} else if (x == 20){
+		System.out.println("x的值是20");
+	} else if (x == 30){
+		System.out.println("x的值是30");
+  	} else if (x == 40){
+  		System.out.println("x的值是40");
+	} else {
+		System.out.println("这是else语句");
+	}
+	}
+}
